47bb173abb151b30333ea8eac94fe9ad6bc9aabd
akopeikin <andrew.kopeikin@usma.edu>
2018-02-28 09:10:24 -0500

    added a means of odroid-basestation communication w/automated snapshot capability

--- usma_files/Video/snap-odroid-image.sh
+++ usma_files/Video/snap-odroid-image.sh
@@ -6,12 +6,12 @@
 PICFOLDER='/home/rrc/odroid_images'
 DATE=`date '+%Y-%m-%d-%H-%M-%S'`;
 
-curl -s -f -m 5 http://odroid:odroid@192.168.11.125:8090/?action=snapshot > "$PICFOLDER/camera-$DATE.jpeg"
+curl -s -f -m 5 http://odroid:odroid@192.168.11.$1:8090/?action=snapshot > "$PICFOLDER/waypoint$2-$DATE.jpeg"
 
-if [ -s "$PICFOLDER/camera-$DATE.jpeg" ]; then
-    cp -f "$PICFOLDER/camera-$DATE.jpeg" /tmp/.camera.jpeg
-    convert "/tmp/.camera.jpeg" -gravity northwest -pointsize 16 -undercolor 'rgba(255,155,0,0.6)' -fill black -annotate +5+5 "ODROID Snapshot $DATE" "$PICFOLDER/camera-$DATE.jpeg"
+if [ -s "$PICFOLDER/waypoint$2-$DATE.jpeg" ]; then
+    cp -f "$PICFOLDER/waypoint$2-$DATE.jpeg" /tmp/.camera.jpeg
+    convert "/tmp/.camera.jpeg" -gravity northwest -pointsize 16 -undercolor 'rgba(255,155,0,0.6)' -fill black -annotate +5+5 "UAS#$1 Snapshot $DATE Waypoint $2 " "$PICFOLDER/waypoint$2-$DATE.jpeg"
 else
-    logger -t $0 "unable to save $PICFOLDER/camera-$DATE.jpeg"
+    logger -t $0 "unable to save $PICFOLDER/waypoint$2-$DATE.jpeg"
     sudo systemctl restart mjpeg_streamer.service
 fi
---
+++ usma_files/Video/waypoint_client.py
@@ -0,0 +1,32 @@
+import socket
+import sys
+
+waypoints = []
+
+#create a TCP/IP socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+#THE SENSOR STATION MUST BE .201
+#connect the socket to the port where the server is listening
+server_address = ('192.168.11.203',10000)
+print >>sys.stderr, 'connecting to %s port %s' % server_address
+sock.connect(server_address)
+
+try:
+    #send data
+    message = '125 4'
+    print >>sys.stderr, 'sending %s' % message
+    sock.sendall(message)
+
+    #look for response
+    amount_received = 0
+    num_symbols = 32
+    delta = 32
+    while delta == num_symbols:
+        data = sock.recv(num_symbols)
+        delta = len(data)
+        print >>sys.stderr,'received %s' % data
+
+finally:
+    print >>sys.stderr, 'closing socket'
+    sock.close()
