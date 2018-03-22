97b247293c9cbac127cc71dd3e897d25ea644548
andrew.kopeikin_usma.edu <andrew.kopeikin@usma.edu>
2018-03-22 11:55:24 -0400

    updated server

--- usma_files/Video/waypoint_client.py
+++ usma_files/Video/waypoint_client.py
@@ -8,20 +8,20 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 #THE SENSOR STATION MUST BE .201
 #connect the socket to the port where the server is listening
-server_address = ('192.168.11.203',10000)
+server_address = ('127.0.0.1',10000)
 print >>sys.stderr, 'connecting to %s port %s' % server_address
 sock.connect(server_address)
 
 try:
     #send data
-    message = '125 4'
+    message = '1 2 3 4 5 6'
     print >>sys.stderr, 'sending %s' % message
     sock.sendall(message)
 
     #look for response
     amount_received = 0
-    num_symbols = 32
-    delta = 32
+    num_symbols = 64
+    delta = 64
     while delta == num_symbols:
         data = sock.recv(num_symbols)
         delta = len(data)
--- usma_files/Video/waypoint_server.py
+++ usma_files/Video/waypoint_server.py
@@ -3,7 +3,7 @@ import sys
 import subprocess
 
 #create a list of waypoints that must be visited 
-waypoints = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]
+waypoints = dict()
 
 #updates the waypoints based on the quads updates
 def update_waypoints(data,quad_num):
@@ -31,7 +31,7 @@ def listen():
     sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 
     #bind the socket to the port. SENSOR STATION IS 203!!
-    server_address = ('192.168.11.203',10000)
+    server_address = ('127.0.0.1',10000)
     print >>sys.stderr, 'starting up on %s port %s' % server_address
     sock.bind(server_address)
 
@@ -39,25 +39,27 @@ def listen():
     sock.listen(10)
     active_connections = 0
     while True:
-        print >>sys.stderr, 'waiting for a connection'
+        print >>sys.stderr, 'Waiting for a connection \n'
         connection, client_address = sock.accept()
         active_connections += 1
         try:
-            quad_num = str(client_address[0]).split('.')[3]
-            print >>sys.stderr, 'connection from UAS#%s on port %s' % (quad_num,client_address[1])
+            #quad_num = str(client_address[0]).split('.')[3]
+            #print >>sys.stderr, 'connection from UAS#%s on port %s' % (quad_num,client_address[1])
         
             while True:
                 data=connection.recv(128)
                 if (len(data) > 1):
-                    print >>sys.stderr, 'Message received from UAS#%s: %s' % (quad_num, data)
+                    #print >>sys.stderr, 'Message received from UAS#%s: %s' % (quad_num, data)
                     #update_waypoints(data,quad_num)
-                    print(data)
-                    print >>sys.stderr, 'sending updated waypoint list back to UAS#',quad_num
+                    print >>sys.stderr, 'Connection from UAS#%s on Port %s' % (data[:2],client_address[1])
+                    newdata = data[2:]
+                    print(newdata +"\n")
+                    #print >>sys.stderr, 'sending updated waypoint list back to UAS#',quad_num
                     
-                    #senall argument must be string or buffer, not a list
-                    connection.sendall(list2string(waypoints))
+                    #sendall argument must be string or buffer, not a list
+                    connection.sendall("Index to Start: " + newdata[-1:])
                 else:
-                    print >>sys.stderr, 'no more data from UAS#', quad_num
+                    #print >>sys.stderr, 'no more data from UAS#', quad_num
                     break
                 
         finally:
