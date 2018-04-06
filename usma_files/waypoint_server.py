e9c0dab15363c3de929e32c3688615259209809f
andrew.kopeikin_usma.edu <andrew.kopeikin@usma.edu>
2018-04-06 11:05:13 -0400

    change

--- usma_files/waypoint_server.py
+++ usma_files/waypoint_server.py
@@ -3,7 +3,7 @@ import sys
 import subprocess
 
 #create a list of waypoints that must be visited 
-waypoints = dict()
+finishedwp = set([])
 behaviorflag = False
 
 #updates the waypoints based on the quads updates
@@ -27,19 +27,21 @@ def list2string(list1):
     return output
     
 def listen():
-    global waypoints
+    global finishedwp
     global behaviorflag
     #create a TCP/IP socket
     sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 
     #bind the socket to the port. SENSOR STATION IS 203!!
-    server_address = ('127.0.0.1',10000)
+    #192.168.11.202
+    server_address = ('192.168.11.202',10000)
     print >>sys.stderr, 'Starting up on %s port %s...' % server_address
     sock.bind(server_address)
 
     #Listen for incoming connections. You can increase this but you must also allow threading.
     sock.listen(10)
     active_connections = 0
+    savedata = None
     while True:
         print >>sys.stderr, 'Waiting for a connection... \n'
         connection, client_address = sock.accept()
@@ -50,24 +52,27 @@ def listen():
         
             while True:
                 data=connection.recv(1024)
+                print("Connected!")
                 #newdata = eval(data)
                 if (len(data) > 1):
                     newdata = eval(data)
-                    #print >>sys.stderr, 'Message received from UAS#%s: %s' % (quad_num, data)
-                    #update_waypoints(data,quad_num)
+                    savedata = eval(data)
                     print >>sys.stderr, 'Connection from UAS#%s on Port %s' % (newdata[0],client_address[1])
-              
                     print("Working WP List: " + str(newdata[1]))
                     print("Finished WP: " + str(newdata[2]))
+                    finishedwp.add(newdata[2])
+                    print("Finished WP Set: " + str(finishedwp))
                     print("At Index: " + str(newdata[3]))
-                    print("Flag: " + str(newdata[4]))
-                    #print("\n")
-                    #print >>sys.stderr, 'sending updated waypoint list back to UAS#',quad_num
                     
                     #sendall argument must be string or buffer, not a list
-                    connection.sendall(str(behaviorflag))
+                    print("Sending back a message...")
+                    #sendbackmsg = [newdata[4],newdata[3]]
+                    connection.sendall(str(finishedwp))
+
                 else:
                     #print >>sys.stderr, 'no more data from UAS#', quad_num
+                    #sendbackmsg = [savedata[4],savedata[3]]
+                    #connection.sendall(str(sendbackmsg))
                     break
                 
         finally:
