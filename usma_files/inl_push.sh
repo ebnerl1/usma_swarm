8fee4a198f76ce200b9ad6f071a8b5c83b0e9954
andrew.kopeikin <andrew.kopeikin@usma.edu>
2019-03-01 16:45:11 -0500

    changed flags serverside

--- usma_files/inl_push.sh
+++ usma_files/inl_push.sh
@@ -9,7 +9,7 @@
 
 # A smaller list for testing
 
-declare -a arr=("192.168.11.111" "192.168.11.113" "192.168.11.115" "192.168.11.120" "192.168.11.121" "192.168.11.122" "192.168.11.123" "192.168.11.125") 
+declare -a arr=("192.168.11.121") 
 #declare -a arr=("192.168.11.111")
 
 # Below is the previous commit of the master branch before the EOY pull request 
