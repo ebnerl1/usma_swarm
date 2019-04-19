37b9d6cf951814ab446f1c681dcba4a241d8eb29
ConnerRussell <conner.russell@usma.edu>
2019-04-19 15:24:29 -0400

    Added line 215 and modified csv_to_kml converter

--- usma_files/serverSide/csv_to_kml.py
+++ usma_files/serverSide/csv_to_kml.py
@@ -12,19 +12,19 @@ def conver(path):
 
     with open(path) as csv_file:
 
-    csv_reader = csv.reader(csv_file, delimiter = ',')
-    for row in csv_reader:
-        lat = row[3]
-        lon = row[4]
-        rad_info = row[7]
-        row_info = str(i) + ',' + str(lat) + ',' + str(lon) + ',' + str(rad_info)
-        row_info = row_info.split(',')
+        csv_reader = csv.reader(csv_file, delimiter = ',')
+        for row in csv_reader:
+            lat = row[3]
+            lon = row[4]
+            rad_info = row[7]
+            row_info = str(i) + ',' + str(lat) + ',' + str(lon) + ',' + str(rad_info)
+            row_info = row_info.split(',')
 
-        if row_info[0] == '0':
-            start_lat = row_info[2]
-        i = i +1
+            if row_info[0] == '0':
+                start_lat = row_info[2]
+            i = i +1
 
-    csv_file.close()
+        csv_file.close()
 
     dist_width = 2
     dist_height = 2
@@ -41,37 +41,37 @@ def conver(path):
 
     with open(path) as csv_file:
 
-    csv_reader = csv.reader(csv_file, delimiter = ',')
+        csv_reader = csv.reader(csv_file, delimiter = ',')
     
-    for row in csv_reader:
-        lat = float(row[3])
-        lon = float(row[4])
-        rad_info = float(row[7])
-        row_info = str(i) + ',' + str(lat) + ',' + str(lon) + ',' + str(rad_info)
-        i = i + 1
-        row_info = row_info.split(',')
+        for row in csv_reader:
+            lat = float(row[3])
+            lon = float(row[4])
+            rad_info = float(row[7])
+            row_info = str(i) + ',' + str(lat) + ',' + str(lon) + ',' + str(rad_info)
+            i = i + 1
+            row_info = row_info.split(',')
         
-        top_left_x = (lon - width/2)
-        top_left_y = (lat + height/2)
-        bottom_left_x = (lon - width/2)
-        bottom_left_y = (lat - height/2)
-        bottom_right_x = (lon + width/2)
-        bottom_right_y = (lat- height/2)
-        top_right_x = (lon + width/2)
-        top_right_y = (lat + height/2)
+            top_left_x = (lon - width/2)
+            top_left_y = (lat + height/2)
+            bottom_left_x = (lon - width/2)
+            bottom_left_y = (lat - height/2)
+            bottom_right_x = (lon + width/2)
+            bottom_right_y = (lat- height/2)
+            top_right_x = (lon + width/2)
+            top_right_y = (lat + height/2)
 
-        pol = kml.newpolygon(name = row_info[0], outerboundaryis = ([(top_left_x,top_left_y), (top_right_x,top_right_y), (bottom_right_x,bottom_right_y), (bottom_left_x,bottom_left_y),(top_left_x,top_left_y)]))
+            pol = kml.newpolygon(name = row_info[0], outerboundaryis = ([(top_left_x,top_left_y), (top_right_x,top_right_y), (bottom_right_x,bottom_right_y), (bottom_left_x,bottom_left_y),(top_left_x,top_left_y)]))
 
-        if (rad_info < 20):
-            pol.style.polystyle.color = simplekml.Color.changealphaint(100, simplekml.Color.green)
+            if (rad_info < 20):
+                pol.style.polystyle.color = simplekml.Color.changealphaint(100, simplekml.Color.green)
 
-        elif (70 >= rad_info > 20):
-            pol.style.polystyle.color = simplekml.Color.changealphaint(100, simplekml.Color.yellow)
+            elif (70 >= rad_info > 20):
+                pol.style.polystyle.color = simplekml.Color.changealphaint(100, simplekml.Color.yellow)
 
-        else:
-            pol.style.polystyle.color = simplekml.Color.changealphaint(100, simplekml.Color.red)
+            else:
+                pol.style.polystyle.color = simplekml.Color.changealphaint(100, simplekml.Color.red)
        
-    csv_file.close()
+        csv_file.close()
 
     kml.save("/home/osrf/Dropbox/forAtak.kml")
     print("KML file updated in DropBox. Ready for viewing in ATAK.")
