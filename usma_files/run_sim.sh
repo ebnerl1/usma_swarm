7ee5838fe27841ad624398819598c683e3f7b6e2
andrew.kopeikin <andrew.kopeikin@usma.edu>
2019-04-22 12:49:03 -0400

    Adjusted wp server and run_sim behavior for SRNL

--- usma_files/run_sim.sh
+++ usma_files/run_sim.sh
@@ -3,4 +3,4 @@ cd
 multi-sitl-start.bash -B -LSRNL -vArduCopter 2
 fti.py -d sitl_bridge_1 -z &
 gnome-terminal -e "bash -c 'echo network device sitl_bridge_1 | swarm_commander.py;$SHELL'"
-~/usma_swarm/usma_files/serverSide/runServerSide.sh
+#gnome-terminal -e "bash -c '~/usma_swarm/usma_files/serverSide/runServerSide.sh;$SHELL'"
