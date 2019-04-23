b23f437f6c9de15277a870d743289f1f2411fb6b
I-am-Batman <clmjr78@gmail.com>
2019-04-23 10:37:25 -0400

    Bugfix -> seperated launch line from land line in wpduplicate

--- usma_files/blessed_files/wpDuplicate.py
+++ usma_files/blessed_files/wpDuplicate.py
@@ -42,10 +42,26 @@ def main():
     x1 = float(launchfile[7])
     y1 = float(launchfile[9])
     number_of_drones = int(launchfile[11])
+    
+    ax0 = float(launchfile[13])
+    ay0 = float(launchfile[15])
+    ax1 = float(launchfile[17])
+    ay1 = float(launchfile[19])
+
+    bx0 = float(launchfile[21])
+    by0 = float(launchfile[23])
+    bx1 = float(launchfile[25])
+    by1 = float(launchfile[27])
 
     #Create the points 
     xs=np.linspace(x0,x1,number_of_drones)
     ys=np.linspace(y0,y1,number_of_drones)
+
+    axs=np.linspace(ax0,ax1,number_of_drones)
+    ays=np.linspace(ay0,ay1,number_of_drones)
+
+    bxs=np.linspace(bx0,bx1,number_of_drones)
+    bys=np.linspace(by0,by1,number_of_drones)
 
     #print them for error checking
     #for i in range(len(xs)):
@@ -84,6 +100,12 @@ def main():
         #Dynamically allocated coordinates
         coordX = str(xs[i])
         coordY = str(ys[i])
+
+        acX = str(axs[i])
+        acY = str(ays[i])
+
+        bcX = str(bxs[i])
+        bcY = str(bys[i])
         
         #Changing LAUNCH coordinates
         newWaypoint3[8] = coordX
@@ -100,14 +122,14 @@ def main():
         
         
         #Changing LAND A coordinates
-        newWaypoint21[8] = coordX
-        newWaypoint21[9] = coordY
-        newWaypoint23[8] = coordX
-        newWaypoint23[9] = coordY
-        newWaypoint25[8] = coordX
-        newWaypoint25[9] = coordY
-        newWaypoint26[8] = coordX
-        newWaypoint26[9] = coordY
+        newWaypoint21[8] = acX
+        newWaypoint21[9] = acY
+        newWaypoint23[8] = acX
+        newWaypoint23[9] = acY
+        newWaypoint25[8] = acX
+        newWaypoint25[9] = acY
+        newWaypoint26[8] = acX
+        newWaypoint26[9] = acY
 
         lines[22][0] = "\t".join(newWaypoint21)
         lines[24][0] = "\t".join(newWaypoint23)
@@ -116,14 +138,14 @@ def main():
 
 
         #Changing LAND B coordinates
-        newWaypoint29[8] = coordX
-        newWaypoint29[9] = coordY
-        newWaypoint31[8] = coordX
-        newWaypoint31[9] = coordY
-        newWaypoint33[8] = coordX
-        newWaypoint33[9] = coordY
-        newWaypoint34[8] = coordX
-        newWaypoint34[9] = coordY
+        newWaypoint29[8] = bcX
+        newWaypoint29[9] = bcY
+        newWaypoint31[8] = bcX
+        newWaypoint31[9] = bcY
+        newWaypoint33[8] = bcX
+        newWaypoint33[9] = bcY
+        newWaypoint34[8] = bcX
+        newWaypoint34[9] = bcY
 
         lines[30][0] = "\t".join(newWaypoint29)
         lines[32][0] = "\t".join(newWaypoint31)
