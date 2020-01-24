61710cafa209e8e0a9ab6dc891b9790e32e2b945
andrew.kopeikin <andrew.kopeikin@usma.edu>
2020-01-24 14:24:51 -0500

    Updated simulation script

--- usma_files/run_sim.sh
+++ usma_files/run_sim.sh
@@ -3,9 +3,42 @@
 # Script designed to launch all the necessary windows and programs to run a simulation
 # To change num quads, change the number after vArduCopter
 # To change location, swap out the location tag after -B (i.e. "-B -LSRNL")
+# When you're done with the sim, press enter in the terminal and it will shutdown the drones
+
 cd
-multi-sitl-start.bash -B -LSRNL -vArduCopter 2
-fti.py -d sitl_bridge_1 -z &
-gnome-terminal -e "bash -c 'echo network device sitl_bridge_1 | swarm_commander.py;$SHELL'"
+
+echo "Creating drones"
+
+multi-sitl-start.bash -B -LRiverCourt -vArduCopter 1
+
+clear
+
+echo "Starting fti"
+
+gnome-terminal -x bash -c "fti.py -d sitl_bridge_1 -z; exit; exec bash"
+
+echo "Starting Swarm Commander"
+
+echo "Next: "
+echo "    1) Run MavProxy"
+echo "      - wp list"
+echo "      - fence list"
+echo "      - rally list"
+echo "    2) Toggle flight ready"
+echo "    3) Armed"
+echo "    4) Auto"
+
+gnome-terminal -x bash -c "echo network device sitl_bridge_1 | swarm_commander.py; exit; exec bash"
+
 #gnome-terminal -e "bash -c '~/usma_swarm/usma_files/serverSide/runServerSide.sh;$SHELL'"
 # Line above can be uncommented to launch the wpServer at the same time
+
+# Wait for user to press enter before destroying drones
+read varname
+
+echo "Destroying Drones"
+
+multi-sitl-cleanup.bash
+
+clear
+
