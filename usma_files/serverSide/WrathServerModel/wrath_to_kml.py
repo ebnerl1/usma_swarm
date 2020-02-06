a966c8dfd44ee83052e36add280ec21e67ec9e93
coopercone@yahoo.com <andrew.kopeikin@usma.edu>
2020-02-06 13:25:47 -0500

    Added graph support in kml

--- usma_files/serverSide/WrathServerModel/wrath_to_kml.py
+++ usma_files/serverSide/WrathServerModel/wrath_to_kml.py
@@ -7,6 +7,8 @@ import time
 # add point, line segment
 # save 
 
+from Collections import Graph
+
 kml = None
 
 #building kml
@@ -22,6 +24,12 @@ def addPoint(point):
 def addLine(start,end):
 	ls = kml.newlinestring()
 	ls.coords = [start,end]
+
+def addGraph(graph):
+	for vertex in graph.vertices:
+		addPoint(vertex.coord)
+	for edge in graph.getEdges():
+		addLine(edge.start.coord, edge.end.coord)
 
 # need to save it to a destination for viewing
 def save():
